ef52ea63b5b7e10a3424f77d35edfe6e9f4ad24b
Siddharthvj23 <vijaysiddharth23@gmail.com>
2024-03-20 17:53:55 +0530

    payment method add

--- bookmyshow/src/pages/BookShow.jsx
+++ bookmyshow/src/pages/BookShow.jsx
@@ -5,12 +5,14 @@ import { getShowById } from '../apicalls/show'
 import { useNavigate, useParams } from 'react-router-dom'
 import { message, Card, Row, Col, Button } from 'antd'
 import moment from 'moment'
+import StripeCheckout from 'react-stripe-checkout'
 
 const BookShow = () => {
     const { user } = useSelector((state) => state.user)
     const dispatch = useDispatch()
     const [show, setShow] = useState()
     const [selectedSeats, setSelectedSeats] = useState([])
+
     const params = useParams()
     const navigate = useNavigate()
 
@@ -33,9 +35,9 @@ const BookShow = () => {
     }
 
     const getSeats = () => {
-        let columns = 10
-        let TotalSeats = 200
-        let rows = TotalSeats / columns
+        let columns = 12
+        let TotalSeats = show.totalSeats
+        let rows = Math.ceil(TotalSeats / columns)
 
         return (
             <div className='seats-container'>
@@ -49,22 +51,50 @@ const BookShow = () => {
                     {Array.from(Array(rows).keys()).map((row) => {
                         return Array.from(Array(columns).keys()).map((column) => {
                             let seatNumber = row * columns + column + 1
-                            
+
                             let seatClass = 'seat-btn'
 
-                            if(seatNumber <= TotalSeats)
-                                return(
+                            if (selectedSeats.includes(seatNumber)) {
+                                seatClass += 'selected'
+                            }
+                            if (show.bookedSeats.includes(seatNumber)) {
+                                seatClass += 'booked'
+                            }
+
+                            if (seatNumber <= TotalSeats)
+                                return (
                                     <li>
-                                        <button className={seatClass}></button>
+                                        <button
+                                            className={seatClass}
+                                            onClick={() => {
+                                                if (selectedSeats.includes(seatNumber)) {
+                                                    setSelectedSeats(
+                                                        selectedSeats.filter((curSeatNumber) => curSeatNumber !== seatNumber)
+                                                    )
+                                                } else {
+                                                    setSelectedSeats([...selectedSeats, seatNumber])
+                                                }
+                                            }}>
+                                            {seatNumber}
+                                        </button>
                                     </li>
-                                    )
+                                )
                         })
                     })}
                 </ul>
+                <div className="bottom-card">
+                    <div>
+                        Selected Seats: <span>{selectedSeats.join(", ")}</span>
+                    </div>
+                    <div>
+                        Total Price:{" "}
+                        <span>Rs. {selectedSeats.length * show.ticketprice}</span>
+                    </div>
+                </div>
             </div>
-        )
-    }
-    console.log(getSeats())
+        );
+    };
+
     useEffect(() => {
         getData()
     }, [])
@@ -102,10 +132,20 @@ const BookShow = () => {
                                     </h3>
                                 </div>
                             }
-                            style={{width: '100%'}}
-                            
-                        >{getSeats()}</Card>
-                </Col>
+                            style={{ width: '100%' }}
+
+                        >{getSeats()}
+
+                        {selectedSeats.length > 0 && <StripeCheckout  billingAddress amount={selectedSeats.length * show.ticketprice * 100}
+                                stripeKey='pk_test_51OwMveSCBWExgHuK92erGllMEA7uu15iZsRQPWYXYPakWfDv3hUWkwjp2hr7HQka6NYGq4lAcVRSzQe7R8V1DtGu00i4T6kyUh'>
+                                <div className=' max-w-xl mx-auto'>
+                                    <Button shape='round' size='large' block>
+                                        Pay Now 
+                                    </Button>
+                                </div>    
+                            </StripeCheckout>}
+                        </Card>
+                    </Col>
                 </Row>
             )}
         </>
